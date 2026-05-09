2d5a3dab626200413459a085bf7be85786088b37
Marco Garofalo <marcogarofalo.personal@gmail.com>
2026-05-09 23:14:43 +0100

    tidy up

--- src/test/kotlin/com/example/Fixtures.kt
+++ src/test/kotlin/com/example/Fixtures.kt
@@ -2,15 +2,48 @@ package com.example
 
 import org.http4k.ai.llm.chat.ChatResponse
 import org.http4k.ai.llm.model.Content
-import org.http4k.ai.llm.model.Message
+import org.http4k.ai.llm.tools.ToolRequest
+import org.http4k.ai.model.RequestId
 import org.http4k.ai.model.ResponseId
+import org.http4k.ai.model.ToolName
 import org.http4k.connect.openai.OpenAIModels
 import java.util.*
+import org.http4k.ai.llm.model.Message as LLMMessage
 
 object Answers {
-    object DownKnowHowToRespond {
+    object RequireToolExecution {
+        operator fun invoke(name: ToolName, arguments: Map<String, Any>) = ChatResponse(
+            message = LLMMessage.Assistant(
+                toolRequests = listOf(
+                    ToolRequest(
+                        id = RequestId.of(UUID.randomUUID().toString()),
+                        name = name,
+                        arguments = arguments,
+                    )
+                )
+            ),
+            metadata = ChatResponse.Metadata(
+                id = ResponseId.of(UUID.randomUUID().toString()),
+                model = OpenAIModels.GPT4,
+            )
+        )
+    }
+
+    object TextReply {
+        operator fun invoke(text: String) = ChatResponse(
+            message = LLMMessage.Assistant(listOf(
+                Content.Text(text)
+            )),
+            metadata = ChatResponse.Metadata(
+                id = ResponseId.of(UUID.randomUUID().toString()),
+                model = OpenAIModels.GPT4,
+            )
+        )
+    }
+
+    object DontKnowHowToRespond {
         operator fun invoke() = ChatResponse(
-            message = Message.Assistant(
+            message = LLMMessage.Assistant(
                 listOf(
                     Content.Text("Not sure how to answer that")
                 )
